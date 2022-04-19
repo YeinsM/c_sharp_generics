8608c20961ec5e64d677a7c5c636a3bf77eaf1e4
YeinsM <wildermancera1@gmail.com>
2022-04-19 11:34:03 -0400

    Sample for work with simple stack class for double object

--- YeinsBrainSoftware.StackApp/Program.cs
+++ YeinsBrainSoftware.StackApp/Program.cs
@@ -6,7 +6,28 @@ namespace YeinsBrainSoftware.StackApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            StackDoubles();
+            //StackStrings();
+
+            Console.ReadLine();
         }
-    }
+
+        private static void StackDoubles()
+        {
+            var stack = new SimpleStack();
+            stack.Push(1.2);
+            stack.Push(2.8);
+            stack.Push(3.0);
+
+            double sum = 0.0;
+
+            while (stack.Count > 0)
+            {
+                double item = stack.Pop();
+                Console.WriteLine($"Item: {item}");
+                sum += item;
+            }
+
+            Console.WriteLine($"Sum: {sum}");
+        }
 }
